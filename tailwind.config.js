9ae77b59c71df27ecf7f90fe6d2ea32bbbccff1c
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-23 22:49:33 +0000

    #1 Add theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,16 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#003F63",
+        yellow: "#F2B138",
+      },
+      fontFamily: {
+        Futura: ["Futura", "sans-serif"],
+        Helvetica: ["Helvetica", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-  // mode: "jit",
-  // purge: [
-  //   "./pages/**/*.{js,ts,jsx,tsx}",
-  //   "./src/components/**/*.{js,ts,jsx,tsx}",
-  // ],
 };
