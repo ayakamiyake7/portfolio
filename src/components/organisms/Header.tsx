9cbd5cb9ae6724540402104783bbda276e68891e
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-25 21:33:42 +0000

    #1 In the middle of creating header.

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -2,19 +2,39 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="container mx-auto py-8 md:flex justify-between items-center">
-      <a href="/" className="hover:opacity-80 transition-all">
-        <img src="/logo.svg" alt="logo" />
+    <header className="container mx-auto py-8 flex justify-between items-center">
+      <a
+        href="/"
+        className="text-yellow fontFamily-futura text-xl hover:opacity-80 transition-all"
+      >
+        Ayaka Miyake
       </a>
-      <nav className="md:flex">
+
+      <span className="lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h8m-8 6h16"
+          />
+        </svg>
+      </span>
+      <nav className="md:block lg:flex">
         <a
-          className="font-bold text-lg fontFamily-Futura hover:opacity-80 transition-all mr-8"
+          className="md:block font-bold text-lg fontFamily-Futura hover:opacity-80 transition-all mr-8"
           href="#aboutMe"
         >
           About me
         </a>
         <a
-          className="font-bold text-lg fontFamily-Futura hover:opacity-80 transition-all"
+          className="md:block font-bold text-lg fontFamily-Futura hover:opacity-80 transition-all"
           href="#project"
         >
           Projects
