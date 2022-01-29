8faabb78bacf1ef4ae5cb452bd714fb711b02425
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-29 23:14:43 +0000

    Create about section.

---
+++ src/components/templates/AboutMe.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <>
+      <section className="container mx-auto flex">
+        <div className="w-3/5">
+          <p className="text-lg tracking-widest">
+            I’m a front-end developer from Japan, currently based in London. In
+            the past, I have worked at web design companies and created some web
+            sites with my team members while working in Japan. I love to learn
+            new things.
+          </p>
+        </div>
+        <div className="w-2/5">img</div>
+      </section>
+    </>
+  );
+};
+
+export default AboutMe;
