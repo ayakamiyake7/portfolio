7aa80496d2c3e3e0b055cf847f082f86a72fb002
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-30 21:23:36 +0000

    #21 Create Title component.

--- src/components/templates/AboutMe.tsx
+++ src/components/templates/AboutMe.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Image from "next/image";
 import ProfileImage from "../../../public/images/image-profile.png";
 import SectionTitle from "../atoms/SectionTitle";
+import Title from "../atoms/Title";
 
 const AboutMe = () => {
   return (
     <>
       <section className="container mx-auto mb-20 px-4 md:px-0 md:flex md:justify-between">
-        <div className="w-full md:w-3/5 mb-10">
+        <div className="w-full md:w-3/5 mb-10 md:mb-0">
           <SectionTitle title="About me" />
           <p className="tracking-widest text-base leading-8 mb-6 md:text-lg">
             I’m a front-end developer from Japan, currently based in London. In
@@ -15,7 +16,8 @@ const AboutMe = () => {
             sites with my team members while working in Japan. I love to learn
             new things.
           </p>
-          <h3>Skills</h3>
+
+          <Title title="Skills" />
           <ul className="text-base flex flex-wrap">
             <li className="w-1/2 md:w-1/3 relative pl-4 before:w-3 before:h-3 before:bg-yellow before:inline-block before:absolute before:left-0 before:top-0 before:bottom-0 before:m-auto">
               HTML5 / CSS3
