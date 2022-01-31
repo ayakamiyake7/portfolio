157ca0277cd8e18458aaa04ad025b8ebcef36e00
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-01-31 13:09:50 +0000

    #25 Refecter code.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 import Layout from "../src/components/layout/Layout";
-import Footer from "../src/components/organisms/Footer";
-import Header from "../src/components/organisms/Header";
 import AboutMe from "../src/components/templates/AboutMe";
 import MainVisual from "../src/components/templates/MainVisual";
+import Projects from "../src/components/templates/Projects";
 
 export default function Home() {
   return (
     <Layout>
       <MainVisual />
       <AboutMe />
+      <Projects />
     </Layout>
   );
 }
---
+++ src/components/templates/Projects.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SectionTitle from "../atoms/SectionTitle";
+
+const Projects = () => {
+  return (
+    <section className="container mx-auto mb-20 px-4 md:px-0">
+      <SectionTitle title="Projects" />
+    </section>
+  );
+};
+
+export default Projects;
