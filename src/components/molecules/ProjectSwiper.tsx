dce98ea12b6e5f8f17ef6aec0ff587ab5746fd80
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-02-06 21:51:33 +0000

    #50 Import swiper

---
+++ src/components/molecules/ProjectSwiper.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Swiper from "react-id-swiper";
+import "swiper/ss/swiper.css";
+
+const ProjectSwiper = () => {
+  return (
+    <Swiper>
+      <div></div>
+    </Swiper>
+  );
+};
+
+export default ProjectSwiper;
