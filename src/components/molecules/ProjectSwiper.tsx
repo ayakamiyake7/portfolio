4523d78989af16cb24ed71727cd8f644aae8f86b
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-02-07 23:31:39 +0000

    #50 Fix swiper

--- src/components/molecules/ProjectSwiper.tsx
+++ src/components/molecules/ProjectSwiper.tsx
@@ -1,11 +1,43 @@
 import React from "react";
-import Swiper from "react-id-swiper";
-import "swiper/ss/swiper.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import Image from "next/image";
+
+import "swiper/css/bundle";
+import "swiper/css/navigation";
 
 const ProjectSwiper = () => {
+  const SLIDEDATA = [
+    //constants
+    {
+      src: "/images/image-01.png",
+      text: "test",
+    },
+  ];
+
   return (
-    <Swiper>
-      <div></div>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      modules={[Navigation]}
+      className="mySwiper"
+      // onSlideChange={() => console.log("slide change")}
+      // onSwiper={(swiper) => console.log(swiper)}
+    >
+      {SLIDEDATA.map((data, index: number) => {
+        return (
+          <SwiperSlide key={`${index}`}>
+            <Image
+              src={data.src}
+              layout="responsive"
+              width={640}
+              height={400}
+              alt="test_image"
+            />
+            {data.text}
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
   );
 };
--- src/components/templates/Projects.tsx
+++ src/components/templates/Projects.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import SectionTitle from "../atoms/SectionTitle";
 import ProjectImage from "../../../public/images/image-01.png";
+import ProjectSwiper from "../molecules/ProjectSwiper";
 
 const Projects = () => {
   return (
@@ -9,6 +10,7 @@ const Projects = () => {
       id="projects"
       className="container mx-auto mt-20 mb-20 px-4 md:px-0"
     >
+      <ProjectSwiper />
       <SectionTitle title="Projects" />
       <div className="mt-8">
         <Image src={ProjectImage} alt="Toritada" width={480} height={208} />
